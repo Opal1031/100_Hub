5d98b2012c746766c541b297e07de2c56a181b23
Minseo Kim <kimminseo5004@gmail.com>
2024-12-03 14:58:33 +0900

    [Silver V] Title: 단어 나누기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,14 @@
+# 1251번
+
+word = input()
+answer = []
+
+for i in range(1, len(word) - 1):
+    for j in range(i + 1, len(word)):
+        first = "".join(reversed(word[:i]))
+        second = "".join(reversed(word[i:j]))
+        third = "".join(reversed(word[j:]))
+
+        answer.append(first + second + third)
+
+print(sorted(answer)[0])
