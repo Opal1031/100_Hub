eacd645a8982f1dbb03abc21ff7f48a105e782db
Minseo Kim <kimminseo5004@gmail.com>
2025-07-04 15:37:51 +0900

    [Silver III] Title: 영식이의 손가락, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.py
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.py
@@ -1,15 +1,15 @@
 dmg_fin = int(input())
-use_dmg_fin = int(input())
+count = int(input())
 
 if dmg_fin == 1:
-    print(8 * use_dmg_fin)
+    print(8 * count)
 
 elif dmg_fin == 5:
-    print(4 * (use_dmg_fin * 2 + 1))
+    print(4 * (count * 2 + 1))
 
 else:
-    if (use_dmg_fin % 2) == 0:
-        print((4 * use_dmg_fin) + (dmg_fin - 1))
-    
+    if (count % 2) == 0:
+        print((4 * count) + (dmg_fin - 1))
+
     else:
-        print((4 * use_dmg_fin) + (5 - dmg_fin))
+        print((4 * count) + (5 - dmg_fin))
