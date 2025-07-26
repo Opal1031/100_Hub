563f4101cddf4d3684492d84626e03059f40389e
Minseo Kim <kimminseo5004@gmail.com>
2025-07-26 21:31:29 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+num = N
+count = 0
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = b * 10 + c
+
+    count += 1
+
+    if num == N:
+        break
+
+print(count)
