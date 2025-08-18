441b81d8e6f5ddc90dd068e8ed48b32d8ef55820
Minseo Kim <kimminseo5004@gmail.com>
2025-08-18 20:53:27 +0900

    [Silver III] Title: 터렛, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+
+    length = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    if (length == 0):
+        if (r1 == r2):
+            print(-1)
+
+        else:
+            print(0)
+
+    else:
+        if((r1 + r2 == length) or (abs(r1 - r2) == length)):
+            print(1)
+
+        elif((abs(r1 - r2) < length < (r1 + r2))):
+            print(2)
+
+        else:
+            print(0)
