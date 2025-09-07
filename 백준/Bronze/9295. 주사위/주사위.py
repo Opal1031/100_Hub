e24c11f4cd5158bb30c279f5849988fd22efca2a
Minseo Kim <kimminseo5004@gmail.com>
2025-09-07 19:24:16 +0900

    [Bronze IV] Title: 주사위, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9295. 주사위/주사위.py
+++ 백준/Bronze/9295. 주사위/주사위.py
@@ -1,6 +1,8 @@
 import sys
 
-n = int(sys.stdin.readline().strip())
-for x in range(n):
-    temp = list(map(int, sys.stdin.readline().strip().split()))
-    print(f"Case {x + 1}: {temp[0] + temp[1]}")
+T = int(sys.stdin.readline().strip())
+
+for i in range(T):
+    a, b = map(int, sys.stdin.readline().strip().split())
+
+    print("Case %d: %d" %(i + 1, a + b))
