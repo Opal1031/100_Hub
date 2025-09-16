a2308c567db46603c5d2d06b4f9472113bebcf4f
Minseo Kim <kimminseo5004@gmail.com>
2025-09-16 17:15:51 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,13 @@
+ch = input()
+
+if(ch == "SONGDO"):
+    print("HIGHSCHOOL")
+
+elif(ch == "CODE"):
+    print("MASTER")
+
+elif(ch == "2023"):
+    print("0611")
+
+elif(ch == "ALGORITHM"):
+    print("CONTEST")
