36fab3c41437171b611db35c88f258db5ba3da0e
Minseo Kim <kimminseo5004@gmail.com>
2026-01-12 19:59:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 288 ms, Memory: 46504 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+
+for i in range(N):
+    a, b = map(int,sys.stdin.readline().split())
+    arr.append((a,b))
+    
+arr.sort()
+
+for i in range(N):
+    print(arr[i][0], arr[i][1])
