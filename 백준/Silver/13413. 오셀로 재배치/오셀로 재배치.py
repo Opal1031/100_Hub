3ed0cda77b6fbf2ce0bea51b85ae23096538662b
Minseo Kim <kimminseo5004@gmail.com>
2026-02-07 20:49:05 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 1008 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
@@ -0,0 +1,21 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T) :
+    n = int(sys.stdin.readline())
+    
+    cnt_b = 0
+    cnt_w = 0
+    
+    data1 = input()
+    data2 = input()
+    
+    for i in range(n):
+        if data1[i] != data2[i]:
+            if data1[i] == 'B':
+                cnt_b += 1
+            else:
+                cnt_w += 1
+                
+    print(max(cnt_b, cnt_w))
