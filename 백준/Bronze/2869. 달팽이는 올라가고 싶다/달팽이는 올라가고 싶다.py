1d76f0fe87b6fd2540b5f1ad450d95ac51071a47
Minseo Kim <kimminseo5004@gmail.com>
2026-03-23 20:41:40 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,8 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 A, B, V = map(int, input().split())
 
-x = (V - B) / (A - B)
-
-if x == int(x):
-    print(int(x))
-else:
-    print(int(x) + 1)
+print((V - B - 1) // (A - B) + 1)
