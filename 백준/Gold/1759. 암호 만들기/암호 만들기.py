bef4eb4a718ca5224f1798f845a8ed4ea398175c
Minseo Kim <kimminseo5004@gmail.com>
2026-03-27 16:14:49 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def backtracking(start, path):
+    if (len(path) == L):
+        vowel_count = 0
+
+        for char in path:
+            if char in "aeiou":
+                vowel_count += 1
+
+        consonant_count = L - vowel_count
+
+        if (vowel_count >= 1 and consonant_count >= 2):
+            print("".join(path))
+        
+        return
+    
+    for i in range(start, C):
+        path.append(chars[i])
+        backtracking(i + 1, path)
+        path.pop()
+
+L, C = map(int, input().split())
+
+chars = sorted(input().split())
+
+backtracking(0, [])
