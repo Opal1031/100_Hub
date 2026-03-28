a74d57498fcd6ac3defcdca883c82483cde43e6c
Minseo Kim <kimminseo5004@gmail.com>
2026-03-28 17:57:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,7 +1,7 @@
 import sys
+input = sys.stdin.readline
 
-star = "*"
-num = int(sys.stdin.readline())
+N = int(input())
 
-for i in range(num):
-    print(star * (i + 1))
+for i in range(1, N + 1):
+    print("*" * i)
