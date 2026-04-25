15b2b9f7a3faae090e215e7cd527a8bdaaa7416b
Minseo Kim <kimminseo5004@gmail.com>
2026-04-25 20:32:04 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,2 +1,11 @@
 def solution(strlist):
-    return [len(i) for i in strlist]
+    answer = [0] * 52
+    
+    for c in my_string:
+        if ("A" <= c <= "Z"):
+            answer[ord(c) - 65] += 1
+            
+        elif ("a" <= c <= "z"):
+            answer[ord(c) - 71] += 1
+            
+    return answer
